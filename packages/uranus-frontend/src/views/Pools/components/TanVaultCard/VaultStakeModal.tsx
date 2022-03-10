48ba6d5659c9898d3b67cffea9463dd341b448d6
unknown <stf1388@gmail.com>
2022-03-10 21:30:09 +0300

    Update VaultStakeModal.tsx

--- packages/uranus-frontend/src/views/Pools/components/TanVaultCard/VaultStakeModal.tsx
+++ packages/uranus-frontend/src/views/Pools/components/TanVaultCard/VaultStakeModal.tsx
@@ -101,7 +101,7 @@ const VaultStakeModal: React.FC<VaultStakeModalProps> = ({
   const annualRoi = interestBreakdown[3] * pool.earningTokenPrice
   const formattedAnnualRoi = formatNumber(annualRoi, annualRoi > 10000 ? 0 : 2, annualRoi > 10000 ? 0 : 2)
 
-  const getTokenLink = stakingToken.address ? `/swap?outputCurrency=${stakingToken.address}` : '/swap'
+  const getTokenLink = stakingToken.address ? `/#/swap?outputCurrency=${stakingToken.address}` : '/#/swap'
 
   const handleStakeInputChange = (input: string) => {
     if (input) {
